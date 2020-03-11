de2c86372a77ad202f33fc939fb9a6061e1d004b
Alberto Martín López <alberto.martin95@gmail.com>
2020-03-11 12:06:57 +0100

    Now using Swagger v2 libraries

--- src/main/java/es/us/isa/idlreasoner/mapper/OAS2MiniZincVariableMapper.java
+++ src/main/java/es/us/isa/idlreasoner/mapper/OAS2MiniZincVariableMapper.java
@@ -1,10 +1,10 @@
 package es.us.isa.idlreasoner.mapper;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Operation;
-import io.swagger.v3.oas.models.media.Schema;
-import io.swagger.v3.oas.models.parameters.Parameter;
-import io.swagger.v3.parser.OpenAPIV3Parser;
+import io.swagger.models.Swagger;
+import io.swagger.models.Operation;
+import io.swagger.models.parameters.AbstractSerializableParameter;
+import io.swagger.models.parameters.Parameter;
+import io.swagger.parser.SwaggerParser;
 
 import java.io.*;
 import java.util.*;
@@ -14,14 +14,14 @@ import static es.us.isa.idlreasoner.util.IDLConfiguration.*;
 
 public class OAS2MiniZincVariableMapper extends AbstractVariableMapper {
 
-    private OpenAPI openAPISpec;
+    private Swagger openAPISpec;
     private List<Parameter> parameters;
 
     public OAS2MiniZincVariableMapper(String apiSpecificationPath, String operationPath, String operationType, MapperResources mr) {
         super(mr);
         this.specificationPath = apiSpecificationPath;
 
-        openAPISpec = new OpenAPIV3Parser().read(apiSpecificationPath);
+        openAPISpec = new SwaggerParser().read(apiSpecificationPath);
         parameters = getOasOperation(openAPISpec, operationPath, operationType).getParameters(); // NullPointerException would be thrown on purpose, to stop program
 
         try {
@@ -50,15 +50,16 @@ public class OAS2MiniZincVariableMapper extends AbstractVariableMapper {
         Integer intMapping;
 
         for (Parameter parameter : parameters) {
-            Schema<?> schema = parameter.getSchema();
+            String paramType = ((AbstractSerializableParameter)parameter).getType();
+            List<?> paramEnum = ((AbstractSerializableParameter) parameter).getEnum();
 
-            if(schema.getType().equals("boolean")) {
+            if(paramType.equals("boolean")) {
                 var = "var bool: ";
                 mapPSetZero(parameter.getName(), "false");
-            } else if(schema.getEnum() != null) {
-                if (schema.getType().equals("string")) {
+            } else if(paramEnum != null) {
+                if (paramType.equals("string")) {
                     var = "var {";
-                    for (Object o : schema.getEnum()) {
+                    for (Object o : paramEnum) {
                         intMapping = mr.stringIntMapping.get(o.toString());
                         if (intMapping != null) {
                             var += intMapping + ", ";
@@ -70,35 +71,35 @@ public class OAS2MiniZincVariableMapper extends AbstractVariableMapper {
                     var = var.substring(0, var.length()-2); // trim last comma and space
                     var += "}: ";
                     mapPSetZero(parameter.getName(), Integer.toString(mr.stringToIntCounter-1));
-                } else if (schema.getType().equals("integer")) {
+                } else if (paramType.equals("integer")) {
                     var = "var {";
-                    for (Object o : schema.getEnum()) {
+                    for (Object o : paramEnum) {
                         var += o + ", ";
                     }
                     var = var.substring(0, var.length()-2); // trim last comma and space
                     var += "}: ";
-                    mapPSetZero(parameter.getName(), schema.getEnum().get(0).toString());
+                    mapPSetZero(parameter.getName(), paramEnum.get(0).toString());
 //                } else if (schema.getType().equals("number")) {
 //                    // TODO: Manage mapping of float enum
 //                    var = "var float: ";
 //                    mapPSetZero(parameter.getName(), "1");
                 } else {
-                    throw new IllegalArgumentException("The enum parameter type '" + schema.getType() + "' is not allowed for IDLReasoner to work.");
+                    throw new IllegalArgumentException("The enum parameter type '" + paramType + "' is not allowed for IDLReasoner to work.");
                 }
-            } else if(schema.getType().equals("string")) {
+            } else if(paramType.equals("string")) {
                 var = "var 0..10000: "; // If string, add enough possible values (10000)
                 mapPSetZero(parameter.getName(), "1");
-            } else if(schema.getType().equals("integer")) {
+            } else if(paramType.equals("integer")) {
                 var = "var int: ";
                 mapPSetZero(parameter.getName(), "1");
-            } else if (schema.getType().equals("array")) {
+            } else if (paramType.equals("array")) {
                 var = "var 0..10000: "; // If array, treat it as a string, add enough possible values (10000)
                 mapPSetZero(parameter.getName(), "1");
-            } else if (schema.getType().equals("number")) {
+            } else if (paramType.equals("number")) {
                 var = "var int: ";
                 mapPSetZero(parameter.getName(), "1");
             } else {
-                throw new IllegalArgumentException("The parameter type '" + schema.getType() + "' is not allowed for IDLReasoner to work.");
+                throw new IllegalArgumentException("The parameter type '" + paramType + "' is not allowed for IDLReasoner to work.");
             }
             var += origToChangedParamName(parameter.getName())+";\n";
             out.append(var);
@@ -106,10 +107,10 @@ public class OAS2MiniZincVariableMapper extends AbstractVariableMapper {
             varSet = "var 0..1: " + origToChangedParamName(parameter.getName())+"Set;\n";
             out.append(varSet);
 
-            if (parameter.getRequired() != null && parameter.getRequired()) {
+            if (parameter.getRequired()) {
                 mapRequiredVar(requiredVarsOut, parameter);
             }
-            mr.operationParameters.put(parameter.getName(), new AbstractMap.SimpleEntry<>(schema.getType(), parameter.getRequired()!=null ? parameter.getRequired() : false));
+            mr.operationParameters.put(parameter.getName(), new AbstractMap.SimpleEntry<>(paramType, parameter.getRequired()));
         }
 
         out.newLine();
@@ -140,7 +141,7 @@ public class OAS2MiniZincVariableMapper extends AbstractVariableMapper {
         constraintsRedundantSolutions += "constraint ((" + origToChangedParamName(paramName) + "Set==0) -> (" + origToChangedParamName(paramName) + "==" + paramValue + "));\n";
     }
 
-    private static Operation getOasOperation(OpenAPI openAPISpec, String operationPath, String operationType) {
+    private static Operation getOasOperation(Swagger openAPISpec, String operationPath, String operationType) {
         if(operationType.toLowerCase().equals("get"))
             return openAPISpec.getPaths().get(operationPath).getGet();
         if(operationType.toLowerCase().equals("delete"))
@@ -160,11 +161,17 @@ public class OAS2MiniZincVariableMapper extends AbstractVariableMapper {
     }
 
     public static void generateIDLfromIDL4OAS(String apiSpecificationPath, String operationPath, String operationType) throws IOException {
-        OpenAPI oasSpec = new OpenAPIV3Parser().read(apiSpecificationPath);
+        Swagger oasSpec = new SwaggerParser().read(apiSpecificationPath);
         Operation oasOp = getOasOperation(oasSpec, operationPath, operationType);
 
-        List<String> IDLdeps = (List<String>)oasOp.getExtensions().get("x-dependencies");
-        if (IDLdeps != null) {
+//        List<String> IDLdeps = (List<String>)oasOp.getExtensions().get("x-dependencies");
+
+        List<String> IDLdeps = null;
+        try {
+            IDLdeps = (List<String>)oasOp.getVendorExtensions().get("x-dependencies");
+        } catch (Exception e) {} // If the "x-dependencies" extension is not correctly used
+
+        if (IDLdeps != null && IDLdeps.size() != 0) {
             BufferedWriter out = openWriter(IDL_AUX_FILE);
             for (String IDLdep : IDLdeps) {
                 out.append(IDLdep + "\n");
